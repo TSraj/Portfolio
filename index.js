fdac88339b8524cfe1474708bf7a4b8dd46f1419
TSraj <tanvirraj475@gmail.com>
2020-04-20 11:20:50 +0600

    My first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+$(document).ready(function(e){
+    $win = $(window);
+    $navbar = $('#header');
+    $toggle = $('.toggle-button');
+    var width = $navbar.width();
+    toggle__onclick($win,$navbar,width);
+
+
+    //resize event
+    $win.resize(function(){
+        toggle__onclick($win,$navbar,width);
+    });
+
+
+    $toggle.click(function(e){
+        $navbar.toggleClass("toggle-left");
+    });
+
+});
+
+
+function toggle__onclick($win, $navbar, width){
+    if($win.width()<=768){
+        $navbar.css({left:`-${width}px`});
+    }else{
+        $navbar.css({left:'0px'});
+    }
+}
+
+var typed = new Typed('#typed',{
+    strings: [
+        'Traveller',
+        'Foodie',
+        'Photographer',
+        'Software Engineer'
+    ],
+    typeSpeed:50,
+    backSpeed:50,
+    loop: true
+});
+
+var typed_2 = new Typed('#typed_2',{
+    strings: [
+        'Traveller',
+        'Foodie',
+        'Photographer',
+        'Software Engineer'
+    ],
+    typeSpeed:50,
+    backSpeed:50,
+    loop: true
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+    anchor.addEventListener('click',function(e){
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior:'smooth'
+        });
+    });
+});
